3d801de84e1d94b31f69afd793c4c31d37d24e71
kingfree <kingfree@toyama.moe>
2022-02-28 15:07:37 +0800

    peek

--- src/queue.rs
+++ src/queue.rs
@@ -1,10 +1,33 @@
 use core::marker::PhantomData;
 use core::mem::{self, align_of, size_of};
-use core::ptr::{drop_in_place, null_mut};
+use core::ptr::{self, drop_in_place, null_mut};
 use core::sync::atomic::{fence, AtomicBool, AtomicPtr, AtomicUsize, Ordering};
+use std::sync::atomic::compiler_fence;
 extern crate alloc;
 
 const CACHE_LINE_SIZE: usize = 64;
+
+#[cfg(debug_assertions)]
+struct ReentrantGuard {
+    in_section: AtomicBool,
+}
+
+#[cfg(debug_assertions)]
+impl ReentrantGuard {
+    fn new(in_section: &AtomicBool) -> Self {
+        assert!(!in_section.load(Ordering::Relaxed), "Concurrent (or re-entrant) enqueue or dequeue operation detected (only one thread at a time may hold the producer or consumer role)");
+        Self {
+            in_section: AtomicBool::new(true),
+        }
+    }
+}
+
+#[cfg(debug_assertions)]
+impl Drop for ReentrantGuard {
+    fn drop(&mut self) {
+        self.in_section.store(false, Ordering::Relaxed);
+    }
+}
 
 pub struct ReaderWriterQueue<T, const MAX_BLOCK_SIZE: usize = 512> {
     /// (Atomic) Elements are dequeued from this block
@@ -94,28 +117,132 @@ impl<T, const MAX_BLOCK_SIZE: usize> ReaderWriterQueue<T, MAX_BLOCK_SIZE> {
     }
 
     pub fn try_dequeue(&mut self) -> Option<T> {
-        let front_block = self.front_block.load(Ordering::Relaxed);
-        let block_tail = unsafe { (*front_block).local_tail };
-        let block_front = unsafe { (*front_block).front.load(Ordering::Relaxed) };
+        let front_block_ = self.front_block.load(Ordering::Relaxed);
+        let front_block = unsafe { front_block_.as_mut().unwrap() };
+        let block_tail = front_block.local_tail;
+        let mut block_front = front_block.front.load(Ordering::Relaxed);
+
+        let result;
         if block_front != block_tail
-            || block_front
-                != unsafe {
-                    (*front_block).local_tail = (*front_block).tail.load(Ordering::Relaxed);
-                    (*front_block).local_tail
-                }
+            || block_front != {
+                front_block.local_tail = front_block.tail.load(Ordering::Relaxed);
+                front_block.local_tail
+            }
         {
             fence(Ordering::Acquire);
 
+            // non_empty_front_block;
+            {
+                let element =
+                    unsafe { front_block.data.add(block_front * size_of::<T>()) as *mut T };
+                result = Some(unsafe { ptr::read(element) });
+                unsafe { drop_in_place(element) };
+
+                block_front = (block_front + 1) & front_block.size_mask;
+
+                fence(Ordering::Release);
+                front_block.front = AtomicUsize::new(block_front);
+            }
+        } else if front_block_ != self.tail_block.load(Ordering::Relaxed) {
+            fence(Ordering::Acquire);
+
+            let front_block_ = self.front_block.load(Ordering::Relaxed);
+            front_block.local_tail = front_block.tail.load(Ordering::Relaxed);
+            let block_tail = front_block.local_tail;
+            fence(Ordering::Acquire);
+
+            if block_front != block_tail {
+                // goto non_empty_front_block;
+                let element =
+                    unsafe { front_block.data.add(block_front * size_of::<T>()) as *mut T };
+                result = Some(unsafe { ptr::read(element) });
+                unsafe { drop_in_place(element) };
+
+                block_front = (block_front + 1) & front_block.size_mask;
+
+                fence(Ordering::Release);
+                front_block.front = AtomicUsize::new(block_front);
+
+                return result;
+            }
+
+            let next_block_ = front_block.next.load(Ordering::Relaxed);
+            let next_block = unsafe { next_block_.as_mut().unwrap() };
+            let next_block_front = next_block.front.load(Ordering::Relaxed);
+            next_block.local_tail = next_block.tail.load(Ordering::Relaxed);
+            let next_block_tail = next_block.local_tail;
+            fence(Ordering::Acquire);
+
+            assert!(next_block_front != next_block_tail);
+
+            fence(Ordering::Release);
+            let front_block = next_block;
+            self.front_block.store(front_block, Ordering::Relaxed);
+
+            compiler_fence(Ordering::Release);
+
             let element =
-                unsafe { (*front_block).data.add(block_front * size_of::<T>()) as *mut T };
-            // return Some(unsafe { *element });
-        } else if front_block != self.tail_block.load(Ordering::Relaxed) {
-            let element =
-                unsafe { (*front_block).data.add(block_front * size_of::<T>()) as *mut T };
-            // return Some(unsafe { *element });
+                unsafe { front_block.data.add(next_block_front * size_of::<T>()) as *mut T };
+
+            result = Some(unsafe { ptr::read(element) });
+            unsafe { drop_in_place(element) };
+
+            let next_block_front = (next_block_front + 1) & front_block.size_mask;
+
+            fence(Ordering::Release);
+            front_block.front.store(next_block_front, Ordering::Relaxed);
         } else {
-            return None;
+            result = None;
         }
+        result
+    }
+
+    fn peek(&self) -> Option<&T> {
+        #[cfg(debug_assertions)]
+        let _guard = ReentrantGuard::new(&self.dequeuing);
+
+        let front_block_ = self.front_block.load(Ordering::Relaxed);
+        let front_block = unsafe { front_block_.as_mut() }.unwrap();
+        let block_tail = front_block.local_tail;
+        let block_front = front_block.front.load(Ordering::Relaxed);
+
+        if block_front != block_tail
+            || block_front != {
+                front_block.local_tail = front_block.tail.load(Ordering::Relaxed);
+                front_block.local_tail
+            }
+        {
+            fence(Ordering::Acquire);
+            return unsafe {
+                (front_block.data.add(block_front * size_of::<T>()) as *mut T).as_ref()
+            };
+        } else if front_block_ != self.tail_block.load(Ordering::Relaxed) {
+            fence(Ordering::Acquire);
+            let front_block = self.front_block.load(Ordering::Relaxed);
+            let front_block = unsafe { front_block.as_mut() }.unwrap();
+            front_block.local_tail = front_block.tail.load(Ordering::Relaxed);
+            let block_tail = front_block.local_tail;
+            let block_front = front_block.front.load(Ordering::Relaxed);
+            fence(Ordering::Acquire);
+
+            if block_front != block_tail {
+                return unsafe {
+                    (front_block.data.add(block_front * size_of::<T>()) as *mut T).as_ref()
+                };
+            }
+
+            let next_block = front_block.next.load(Ordering::Relaxed);
+            let next_block = unsafe { next_block.as_mut() }.unwrap();
+
+            let next_block_front = next_block.front.load(Ordering::Relaxed);
+            fence(Ordering::Acquire);
+
+            assert!(next_block_front != next_block.tail.load(Ordering::Relaxed));
+            return unsafe {
+                (next_block.data.add(next_block_front * size_of::<T>()) as *mut T).as_ref()
+            };
+        }
+
         None
     }
 
